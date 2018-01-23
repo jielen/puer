0d93ddd3671cef46cc91752c52befbd8032e13e7
jielen <jielenzghsy1@163.com>
2018-01-23 23:56:46 +0800

    add defaultroot fold to github

---
+++ defaultroot/applet/jarsignerAll.bat
@@ -0,0 +1,15 @@
+﻿jarsigner -keystore ufgov.jks -storepass 111111 com.ufgov.smartclient.component_1.0.0.jar  ufgov
+jarsigner -keystore ufgov.jks -storepass 111111 com.ufgov.smartclient.laf_1.0.0.jar  ufgov
+jarsigner -keystore ufgov.jks -storepass 111111 commons-beanutils-1.5.jar  ufgov
+jarsigner -keystore ufgov.jks -storepass 111111 commons-collections-3.2.1.jar  ufgov
+jarsigner -keystore ufgov.jks -storepass 111111 commons-logging.jar  ufgov
+jarsigner -keystore ufgov.jks -storepass 111111 freemarker.jar  ufgov
+jarsigner -keystore ufgov.jks -storepass 111111 hessian-3.2.1.jar  ufgov
+jarsigner -keystore ufgov.jks -storepass 111111 jacob.jar  ufgov
+jarsigner -keystore ufgov.jks -storepass 111111 jasperPrint.jar  ufgov
+jarsigner -keystore ufgov.jks -storepass 111111 jgraph.jar  ufgov
+jarsigner -keystore ufgov.jks -storepass 111111 JoSQL-2.1-new.jar  ufgov
+jarsigner -keystore ufgov.jks -storepass 111111 log4j-1.2.8.jar  ufgov
+jarsigner -keystore ufgov.jks -storepass 111111 poi.jar  ufgov
+jarsigner -keystore ufgov.jks -storepass 111111 WordSupport.jar  ufgov
+jarsigner -keystore ufgov.jks -storepass 111111 workflow-client.jar  ufgov
